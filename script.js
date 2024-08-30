6bdd0de05857c14d4ef0b193cc356f30012d4cee
Tarun Jhamb <tarunjhamb2705@gamil.com>
2024-08-30 16:42:10 +0530

    landing page navbar and bottom section also added

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
-let tl = gsap.timeline();
+function loadingAnimation(){
+        let tl = gsap.timeline();
 
-tl
-.to(".yellow",{
-    top: "-100%",
-    delay: 0.5,
-    duration: 0.5,
-    ease: "expo.out",
-})
-.to("video",{
-    top: "-100%",
-    delay: 0.5,
-    duration: 0.5,
-    ease: "expo.out",
-},"a")
-.to(".loader",{
-    top: "-100%",
-    delay: 0.5,
-    duration: 0.55,
-    ease: "expo.out",
-},"a")
-.to(".loader h1",{
-    color: "black"
-},"a")
+        tl
+        .to(".yellow",{
+            top: "-100%",
+            delay: 0.5,
+            duration: 0.5,
+            ease: "expo.out",
+        })
+        .to("video",{
+            top: "-100%",
+            delay: 0.5,
+            duration: 0.5,
+            ease: "expo.out",
+        },"a")
+        .to(".loader",{
+            top: "-100%",
+            delay: 0.5,
+            duration: 0.55,
+            ease: "expo.out",
+        },"a")
+        .to(".loader h1",{
+            color: "black"
+        },"a")
+}
+
+loadingAnimation()
